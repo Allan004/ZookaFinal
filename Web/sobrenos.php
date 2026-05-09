93008f5e228cd0d4843363f102e8e268eb5b8266
vitoria06 <vitoriacamposss06@gmail.com>
2026-05-09 14:29:07 -0300

    mexi em muita coisa vê se deu margin

---
+++ Web/sobrenos.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nós - ZookaPetShop</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/sobrenos.css">
+</head>
+
+<body>
+
+    <section class="sobre-nos-grid">
+
+        <div class="bloco azul-claro">
+            <div class="card-personagem">
+                <img class="foto-estatica" src="menina-flor-parada.png" alt="Menina com flor na cabeça">
+                <img class="foto-animada" src="menina-flor-acenando.gif" alt="Menina com flor acenando">
+            </div>
+        </div>
+
+
+        <div class="coluna-um"></div>
+        <div class="bloco verde-agua">
+            <div class="card-personagem">
+                <img class="foto-estatica" src="rapaz-careca-parado.png" alt="Rapaz careca com sinal de positivo">
+                <img class="foto-animada" src="rapaz-careca-animado.gif"
+                    alt="Rapaz careca dando sinal positivo piscando">
+            </div>
+        </div>
+
+        <div class="conteudo-texto"></div>
+
+        <div class="conteudo-texto"></div>
+
+        <div class="texto-institucional">
+            <p> A ZookaPetShop nasceu da paixão do grupo HEVA pela causa animal e da conexão especial que cada integrante possui com seus pets. O nome “Zooka” representa acolhimento, cuidado e carinho.</p>
+            <p>O projeto também surgiu da preocupação com animais abandonados e vítimas de maus-tratos, buscando incentivar a proteção, o cuidado e a conscientização sobre o bem-estar animal.</p>
+        </div>
+
+        <div class="texto-titulo-zooka">
+            <h1>Zooka</h1>
+        </div>
+        <div class="bloco laranja">
+            <div class="card-personagem">
+                <img class="foto-estatica" src="rapaz-comprido-parado.png" alt="Rapaz de cabelo comprido e camisa polo">
+                <img class="foto-animada" src="rapaz-comprido-animado.gif" alt="Rapaz de cabelo comprido piscando">
+            </div>
+        </div>
+
+        <div class="bloco salmao">
+            <div class="card-personagem">
+                <img class="foto-estatica" src="menina-oculos-parada.png"
+                    alt="Menina de óculos escuros e cabelo cacheado">
+                <img class="foto-animada" src="menina-oculos-animada.gif" alt="Menina de óculos ajeitando a armação">
+            </div>
+        </div>
+
+        <div class="bloco marrom">
+            <div class="marrom-conteudo">
+                <img class="logo-redondo-z" src="logo-z-redondo.png" alt="Logo Zooka circular">
+
+                <div class="grupo-patinhas-dog">
+                    <img class="patinhas-decoracao" src="pegadas-decoracao.png" alt="Pegadas de cachorro">
+                    <img class="cachorrinho-ilustra" src="cachorrinho-zooka.png"
+                        alt="Ilustração do cachorrinho da Zooka">
+                </div>
+            </div>
+        </div>
+        </div>
+
+    </section>
+
+</body>
+
+</html>
