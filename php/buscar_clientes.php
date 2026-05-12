edc109b72ad17c97031c11bcdcef4d826c8c3a97
Allan004 <allanbelchior04@gmail.com>
2026-05-11 21:57:13 -0300

    alo

--- php/buscar_clientes.php
+++ php/buscar_clientes.php
@@ -12,7 +12,9 @@ $sql = "SELECT id, nome, cpf
         LIMIT 10";
 
 $stmt = $pdo->prepare($sql);
+
 $like = "%$termo%";
+
 $stmt->bindParam(':termo', $like);
 $stmt->execute();
 
@@ -23,38 +25,50 @@ if (!$resultados) {
     exit;
 }
 
-   echo '<table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">CPF</th>
-                    <th scope="col">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>';
+echo '
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nome</th>
+            <th scope="col">CPF</th>
+            <th scope="col">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+';
 
-                    foreach($resultados as $cliente){
+foreach ($resultados as $cliente) {
 
-                        echo"
-                            
-                            <tr>
-                            <th scope='row'>".$cliente['id']."</th>
-                            <td>".$cliente['nome']."</td>
-                            <td>".$cliente['cpf']."</td>
-                            <td>
-                            <a class='btn btn-block' href='clientes_e_pets.php?editar_cliente=1&id_cliente_editar={$cliente['id']}'>
-                            Editar
-                            </a>
-                        </td>
-                        </tr>";
-                        
-                    }; 
+    // Remove tudo que não for número
+    $cpf_limpo = preg_replace('/\D/', '', $cliente['cpf']);
 
-    echo '</tbody>
-        </table>';
+    // Formata CPF
+    $cpf_formatado = preg_replace(
+        "/(\d{3})(\d{3})(\d{3})(\d{2})/",
+        "$1.$2.$3-$4",
+        $cpf_limpo
+    );
 
+    echo "
+        <tr>
+            <th scope='row'>" . htmlspecialchars($cliente['id']) . "</th>
 
-            
+            <td>" . htmlspecialchars($cliente['nome']) . "</td>
 
+            <td>" . htmlspecialchars($cpf_formatado) . "</td>
 
+            <td>
+                <a class='btn btn-block'
+                   href='clientes_e_pets.php?editar_cliente=1&id_cliente_editar=" . urlencode($cliente['id']) . "'>
+                    Editar
+                </a>
+            </td>
+        </tr>
+    ";
+}
+
+echo '
+    </tbody>
+</table>
+';
